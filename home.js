0a63d6ce97094801078aff9f386bd51e7d607e48
Biggie Emmanuel <biggieemmanuel@gmail.com>
2015-02-24 18:59:41 -0500

    got api calls to work

--- home.js
+++ home.js
@@ -7,3 +7,13 @@ $(document).ready(function(){
      });
 //fadeOut removes tag
 });
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://runextbus.heroku.com/stop/Hill%20Center", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // innerText does not let the attacker inject HTML elements.
+    document.getElementById("resp").innerText = xhr.responseText;
+  }
+}
+xhr.send();
