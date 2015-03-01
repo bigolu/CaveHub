dc7fb4029718b413b7e1d414d7dcb4cdb94d05fa
Biggie Emmanuel <biggieemmanuel@gmail.com>
2015-03-01 17:33:21 -0500

    cleaned up code

--- home.js
+++ home.js
@@ -5,13 +5,16 @@ var hoursOfOps = [ "Open from: 3:00pm - 11:00pm",
 	"Open from: 1:00pm -11:00pm",
 	"Open from: 1:00pm -11:00pm",
 	"Open from: 1:00pm - 3:00pm",
-	"Closed"
+	"The Cave is closed."
 ];
 
 var day = new Date();
 
 //display hours of operations
 	document.getElementById("caveHours").innerHTML = hoursOfOps[day.getDay()];
+
+
+//$("html").css('background-color', 'rgba(0, 0, 0, .8)');
 
 
 
